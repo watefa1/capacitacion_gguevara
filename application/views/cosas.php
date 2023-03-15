a5e2a8e111d675c9f2995a7e7a937d2a051ee782
watefa1 <gaby2001@live.com.ar>
2023-03-15 19:29:59 +0000

    Correcciones luego de la revision semanal #2

--- application/views/cosas.php
+++ application/views/cosas.php
@@ -6,14 +6,14 @@ defined('BASEPATH') OR exit('No direct script access allowed');
 	<meta charset="utf-8">
 	<title>Cosas</title>
 </head>
-<body><center>
-<div id="container">
+<body>
+<div>
 	<h1>Alta de Cosas</h1>
 	</div>
 	<form action="form.php" method="post">
 	<p>Nombre de la cosa: <input type="text" name="nombrecosa" /></p>
 	<p>Cantidad: <input type="text" name="cantidadcosa" /></p>
-	<input type="submit" value="registrar cosa">
+	<button>Registrar</button>
 	</form>
 </body>
     <body>
@@ -30,5 +30,4 @@ defined('BASEPATH') OR exit('No direct script access allowed');
                 <td>Deporte</td>
             </tr>
         </table>
-		</center>
 </html>

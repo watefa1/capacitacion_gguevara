7d7adef1a57b9c25a1f1d10ecb81cea7733b5b56
watefa1 <gaby2001@live.com.ar>
2023-06-30 19:52:02 +0000

    mensaje al no loguear

--- application/views/login.php
+++ application/views/login.php
@@ -1,8 +1,40 @@
 <?php defined('BASEPATH') OR exit('No direct script access allowed');
 
 if (isset($_GET['alert']) && $_GET['alert'] == 1): ?>
+    <div id="error-modal">
+        <p>Debes iniciar sesión para acceder a las demás páginas.</p>	
+    </div>
+    <style>
+        #error-modal {
+            background: #f5f5f5;
+            color:  #4ac4a9;
+            position: fixed;
+            top: 50%;
+            left: 50%;
+            transform: translate(-50%, -50%);
+            padding: 20px;
+            text-align: center;
+        }
+
+        #error-modal p {
+            width: 200px;
+            border: 1px solid  #4ac4a9;
+            padding: 1px;
+			margin-bottom: 20px;
+            top: 1;
+            left: 1;
+			border: 2px solid;
+		  }
+    </style>
     <script>
-        alert("Debes iniciar sesión para acceder a las demás páginas.");
+        document.addEventListener("DOMContentLoaded", function() {
+            var errorModal = document.getElementById("error-modal");
+
+            function closeModal() {
+                errorModal.style.display = "none";
+            }
+            setTimeout(closeModal, 5000);
+        });
     </script>
 <?php endif; ?>
 <!DOCTYPE html>
@@ -18,7 +50,6 @@ if (isset($_GET['alert']) && $_GET['alert'] == 1): ?>
         <form action="login" method="post">
             <label for="username"></label>
             <input type="text" class="campos" placeholder="usuario" id="username" name="username" required>
-
             <label for="password"></label>
             <input type="password" class="campos" placeholder="contraseña" id="password" name="password" required>
             <br>
